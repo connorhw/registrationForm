c7a105db1ebd4d41ba9f7633f27a670bc7765313
Connor Wong <connorwong93@gmail.com>
2020-03-05 23:51:21 -0800

    reg form was given a skelton and index js was modified to render form to dom

--- registrationForm.js
+++ registrationForm.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class RegistrationForm extends Component {
+  render () {
+    return (
+      <form className="registration">
+        <h2>Register</h2>
+        <div className="registration__hint">* required field</div>  
+        <div className="form-group">
+          <label htmlFor="name">Name *</label>
+          <input type="text" className="registration__control"
+            name="name" id="name"/>
+        </div>
+        <div className="form-group">
+           <label htmlFor="password">Password *</label>
+           <input type="password" className="registration__control"
+            name="password" id="password"/>
+           <div className="registration__hint">6 to 72 characters, must include a number</div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="repeatPassword">Repeat Password *</label>
+          <input type="password" className="registration__control"
+            name="repeatPassword" id="repeatPassword"/>
+        </div>
+ 
+        <div className="registration__button__group">
+         <button type="reset" className="registration__button">
+             Cancel
+         </button>
+         <button type="submit" className="registration__button">
+             Save
+         </button>
+        </div>
+      </form>
+    )
+  }
 }
 
-export default App;
+export default RegistrationForm;
